0790532842cfb5c192d7f1b92d54ee59ea59d651
maxbi12x <harshitmittal9090@gmail.com>
2023-05-24 01:10:27 +0530

    Added ImagePicker and Coil Dependency

--- app/src/main/java/com/example/mapperapp/dialogs/AddImageDialogFragment.kt
+++ app/src/main/java/com/example/mapperapp/dialogs/AddImageDialogFragment.kt
@@ -1,5 +1,7 @@
 package com.example.mapperapp.dialogs
 
+import android.app.Activity
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
@@ -8,16 +10,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.DialogFragment
-import com.example.mapperapp.activity.MainActivity
+import coil.load
+import coil.transform.RoundedCornersTransformation
+import com.example.mapperapp.R
 import com.example.mapperapp.databinding.DialogFragmentAddImageBinding
 import com.example.mapperapp.models.ImageDetailsModel
 import com.example.mapperapp.viewmodel.AddImageViewModel
+import com.github.drjacky.imagepicker.ImagePicker
+import com.github.drjacky.imagepicker.constant.ImageProvider
 import java.util.*
 
 
+@Suppress("DEPRECATED_IDENTITY_EQUALS")
 class AddImageDialogFragment : DialogFragment() {
 
+
+    private lateinit var launcher: ActivityResultLauncher<Intent>
 
     //    private var drawingId by Delegates.notNull<Int>()
     private var _binding: DialogFragmentAddImageBinding? = null
@@ -35,6 +47,7 @@ class AddImageDialogFragment : DialogFragment() {
         _binding = DialogFragmentAddImageBinding.inflate(inflater, container, false)
         viewModel = AddImageViewModel(requireActivity().application)
         dialog!!.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        setResultListener()
         return binding.root
     }
 
@@ -45,6 +58,7 @@ class AddImageDialogFragment : DialogFragment() {
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
         super.onViewCreated(view, savedInstanceState)
         getArgs()
         if(isAdd){
@@ -68,10 +82,33 @@ class AddImageDialogFragment : DialogFragment() {
         binding.cancel.setOnClickListener{
             dismiss()
         }
+        binding.attachImageButton.setOnClickListener{
+            getNewImageUriFromGallery()
+        }
 
     }
+    private fun setResultListener(){
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val uri: Uri? = result.data?.data
+                // Use the uri to load the image
+                binding.attachedImage.visibility= View.VISIBLE
+                binding.attachedImage.load(uri){
+                    error(R.drawable.delete)
 
+                    transformations(RoundedCornersTransformation(4f))
+                }
+            } else if (result.resultCode == ImagePicker.RESULT_ERROR) {
+                // Use ImagePicker.Companion.getError(result.data) to show an error
+            }
+        }
 
+    }
+    private fun getNewImageUriFromGallery() {
+        (ImagePicker.with(this.requireActivity())
+            //...
+            .provider(ImageProvider.BOTH).createIntentFromDialog { launcher.launch(it) })
+    }
     private fun getArgs() {
         if (arguments != null && arguments?.containsKey(key) == true) {
             Id = requireArguments().getInt(key)
